067bfd3c85674901d8926179a9501784225994f9
SRUSHTI DIPAK BHALEKAR <160705522+srushtibhalekar@users.noreply.github.com>
2026-07-27 23:28:18 +0530

    Add files via upload

---
+++ EXsum.java
@@ -0,0 +1,32 @@
+class EXsum
+{
+    public static void main (String args[])
+ {
+    System.out.println();
+
+    }
+}
+
+/*DATA TYPE 
+1.Primitive 
+
+interger
+float
+charater
+boolean
+
+
+
+
+2.Non primitive
+short
+byte
+
+
+
+
+
+
+
+
+*/
---
+++ output.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class output{
+    public static void main(String args[]){
+        Scanner in= new Scanner(System.in);
+System.out.println("this is my output");
+    }}
