79282766f0e73e1c5c8da84d3693bf34ed935ad5
SRUSHTI DIPAK BHALEKAR <160705522+srushtibhalekar@users.noreply.github.com>
2026-07-30 00:21:44 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,29 @@
+class Switch {
+    public static void main(String args[]){
+
+        int n = 6;
+        switch(n){
+            case 1:
+                System.out.println("This is case 1");
+                break;
+            case 2:
+                System.out.println("This is case 2");
+                break;
+            case 3:
+                System.out.println("This is case 3");
+                break;
+            case 4:
+                System.out.println("This is case 4");
+                break;
+            case 5:
+                System.out.println("This is case 5");
+                break;
+
+            case 6:
+                System.out.println("This is case 6");
+                break;
+            default:
+                System.out.println("This is default case");
+        }
+    }
+}
