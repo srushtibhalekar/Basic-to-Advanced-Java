19d777c959db00ff8c4d20c61df202b4875de47b
SRUSHTI DIPAK BHALEKAR <160705522+srushtibhalekar@users.noreply.github.com>
2026-08-07 20:51:38 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -1,36 +1,34 @@
-public class Demo {
-    public static void main(String[] args) {
+class MethodOverloading {
 
-       /*  int x=6;
-        int y=5;
-        boolean result = x>y;
-        System.out.println(result);*/
+    // Method to add three integers
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
 
-       /* /*int a = 9;
-        int b= 7;
-        boolean result = a!=b;
-        System.out.println(result);
+    // Method to add two integers
+    public int add(int a, int b) {
+        return a + b;
+    }
 
-        //logical operators
-
-        AND operator - && - TRUE if both conditions are true
-        OR operator - || - TRUE if any one condition is true
-        NOT operator - ! - TRUE if the condition is false-
-        T T - T
-        T F - F
-        F T - F
-        F F - F
-
-        eg.
-        x>y && x!=y - true ,above both conditions are true
-        x>y || x!=y - true, if any one condition is true
-        x>y && x==y - false, first condition is true but second condition is false
-        x>y || x==y - true, first condition is true but second condition is false
-        x<y && x!=y - false, first condition is false but second condition is true
-        x<y || x!=y - true, first condition is false but second condition is true
-        x<y && x==y - false, both conditions are false              
-        
+    // Method to add two doubles
+    public double add(double a, double b) {
+        return a + b;
     }
 }
 
+public class Demo {
 
+    public static void main(String[] args) {
+
+        MethodOverloading obj = new MethodOverloading();
+
+        int r1 = obj.add(10, 20);
+        System.out.println("Addition of two integers: " + r1);
+
+        int r2 = obj.add(10, 20, 30);
+        System.out.println("Addition of three integers: " + r2);
+
+        double r3 = obj.add(10.5, 20.5);
+        System.out.println("Addition of two doubles: " + r3);
+    }
+}
