c7078804510ba14035ff8385b8b5cc046dca0d95
SRUSHTI DIPAK BHALEKAR <160705522+srushtibhalekar@users.noreply.github.com>
2026-08-09 23:58:55 +0530

    Add files via upload

---
+++ Reminder.java
@@ -0,0 +1,10 @@
+class Reminder{
+    public static void main (String args[])
+        {
+      int a = 90 ;
+      int b = 4;
+      int result = a / b;
+      System.out.println(result);
+        }
+    
+}
