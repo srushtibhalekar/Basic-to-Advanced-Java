fd2d22ce95e2f6f7abe9ef1c2518cd6559ecf32d
SRUSHTI DIPAK BHALEKAR <160705522+srushtibhalekar@users.noreply.github.com>
2026-08-10 00:14:33 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,11 @@
+class Avergae
+{
+    public static void main(String args[])
+    {
+        int a = 20;
+        int b = 30;
+        int c = 40;
+        int result = (a + b + c) / 3;
+        System.out.println(result);
+    }
+}
---
+++ Simple.java
@@ -0,0 +1,13 @@
+class Simple{
+    public static void main (String []args)
+    {
+        double principal = 5000;
+        double rate = 5;
+        double time = 2;
+
+        double simpleInterest = (principal * rate * time) / 100;
+
+        System.out.println("Simple Interest = " + simpleInterest);
+    }
+} 
+    
