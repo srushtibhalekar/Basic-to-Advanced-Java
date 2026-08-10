1e2d47401f784309112860dfdd3e7dfe45330f88
SRUSHTI DIPAK BHALEKAR <160705522+srushtibhalekar@users.noreply.github.com>
2026-08-10 23:02:19 +0530

    Add files via upload

---
+++ check.java
@@ -0,0 +1,16 @@
+class check{
+    public static void main (String args[])
+    {
+        int a = 20;
+
+        if (a > 0)
+        
+        {
+            System.out.println("The Number is Postive");
+        }
+            else
+            {
+                System.out.println("The Number is Negative");
+            }
+    }
+}
