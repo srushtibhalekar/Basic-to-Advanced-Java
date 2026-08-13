e2c3be241a703db19880f03a2b44a3d567d0ba24
SRUSHTI DIPAK BHALEKAR <160705522+srushtibhalekar@users.noreply.github.com>
2026-08-13 19:37:44 +0530

    Add files via upload

--- For.java
+++ For.java
@@ -1,9 +1,9 @@
-class For { 
-    //for loop is used to execute a block of code repeatedly until a certain condition is met.
-    public static void main(String args[]){
-        for(int i=0;i<5;i++){ //condition of for loop
-            System.out.println("Hello World");
+class For {
+    public static void main (String argd [])
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            System.out.println(i);
         }
     }
 }
-// we want how many times print the number 1 to 100 then use this
