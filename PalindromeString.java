2e497690c84e2c8fe5e0b5f04c97c8dc031dcd00
SRUSHTI DIPAK BHALEKAR <160705522+srushtibhalekar@users.noreply.github.com>
2026-08-16 23:30:37 +0530

    Add files via upload

---
+++ PalindromeString.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        if (str.equalsIgnoreCase(reversed)) {
+            System.out.println("Palindrome String");
+        } else {
+            System.out.println("Not a Palindrome String");
+        }
+
+        sc.close();
+    }
+}
