729eabbbf81b006518a1ded46b5819d74de05021
SRUSHTI DIPAK BHALEKAR <160705522+srushtibhalekar@users.noreply.github.com>
2026-08-17 22:20:01 +0530

    Add files via upload

---
+++ StringCompare.java
@@ -0,0 +1,14 @@
+public class StringCompare {
+
+    public static void main(String[] args) {
+
+        String str1 = "Java";
+        String str2 = "Java";
+
+        if (str1.equals(str2)) {
+            System.out.println("Both strings are equal");
+        } else {
+            System.out.println("Strings are not equal");
+        }
+    }
+}
